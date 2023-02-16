ddfb824e6922000047c839e3cad178b6c4193bb2
root <iherr.9@gmail.com>
2023-02-16 13:43:24 -0800

    corrections to some files as well as task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main of this task is 
+ * under this comments
+ */
+int main(void)
+{
+	char character;
+	printf("Size of a char: %c byte(s)", sizeof(character));
+	int integer;
+	printf("Size of an int: %d byte(s)", sizeof(integer));
+	long long_int;
+	printf("Size of a long int: %lu byte(s)", sizeof(long_int));
+	long long longer_int;
+	printf("Size of a long long int: %lld byte(s)", sizeof(longer_int));
+	float decimal;
+        printf("Size of a float: %f byte(s)", sizeof(decimal));
+
+}
+
