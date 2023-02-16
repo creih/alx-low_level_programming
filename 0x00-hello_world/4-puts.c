2dfe7de034a7880f1e8d14fdfda73e7b5529965b
root <iherr.9@gmail.com>
2023-02-16 13:04:21 -0800

    still struggling with describing main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*main of this file*/
+//main of this file
 int main(void)
 {
+	//here main starts
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
