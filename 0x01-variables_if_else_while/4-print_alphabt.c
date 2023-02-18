4d3ef28e890cd1b846cf576522c3e8e92efdb911
root <iherr.9@gmail.com>
2023-02-18 01:47:19 -0800

    usin or operator correctly

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - printing alphabets without q and e
+ * Return: if is (0) will be a success
+ */
+int main(void)
+{
+	int alphabet;
+	
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if(alphabet == 'q' || alphabet == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(alphabet);
+		}
+	}
+	putchar('\n');
+	return 0;
+}
