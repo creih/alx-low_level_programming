40a840d5482243802d998478173b8eb38ed4dfa6
root <iherr.9@gmail.com>
2023-02-18 00:53:18 -0800

    task 2 finished about printing alphabet lowercased

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main here we' ll print alphabets 
+ * Return: on success will be (0)
+ */
+int main(void)
+{
+	int character;
+	
+	for (character = 'a'; character <= 'z'; character ++)
+	{
+		putchar(character);
+	}
+	putchar('\n');
+	return (0);
+}
