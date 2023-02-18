ebe832f6347f2757f43ab18f49868d87077ba81b
root <iherr.9@gmail.com>
2023-02-18 04:53:09 -0800

    removed space before line 20

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,16 @@ int main(void)
 	int digit_0;
 	int digit_1;
 
-	for (digit_0 = 0; digit_0 <10; digit_0++)
+	for (digit_0 = 0; digit_0 < 10; digit_0++)
 	{
 		for (digit_1 = digit_0 + 1; digit_1 < 10; digit_1++)
 		{
-			putchar((digit_0 % 10)+48);
+			putchar((digit_0 % 10) + 48);
 			putchar(48 + (digit_1 % 10));
 			if (digit_0 < 8)
 			{
 				putchar(',');
-                        	putchar(' ');
+				putchar(' ');
 			}
 		}
 	}
