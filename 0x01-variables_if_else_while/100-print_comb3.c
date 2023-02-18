5e9570a621f3d21f872febf2166ee92d66944ab7
root <iherr.9@gmail.com>
2023-02-18 04:49:37 -0800

    advanced 100 print 2 digit condition all unique

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - printing 2 digit combo
+ * Return: (0 will show the success of our code)
+ */
+int main(void)
+{
+	int digit_0;
+	int digit_1;
+
+	for (digit_0 = 0; digit_0 <10; digit_0++)
+	{
+		for (digit_1 = digit_0 + 1; digit_1 < 10; digit_1++)
+		{
+			putchar((digit_0 % 10)+48);
+			putchar(48 + (digit_1 % 10));
+			if (digit_0 < 8)
+			{
+				putchar(',');
+                        	putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
