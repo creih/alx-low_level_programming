18467c61bb80d662c302a11fdc7befa0a6e61eff
root <iherr.9@gmail.com>
2023-02-18 00:39:01 -0800

    rectified space errors and output msg on n less than 6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	rem = n%10;
+	rem = n % 10;
 	if (n > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, rem);
@@ -24,9 +24,9 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, rem);
 	}
-	else if (n < 6 && n!=0)
+	else if (n < 6 && n != 0)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, rem);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rem);
 	}
 	return (0);
 }
