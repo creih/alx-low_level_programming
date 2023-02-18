039883c3d48a540b2398fc73e516c3274eaa0333
root <iherr.9@gmail.com>
2023-02-18 11:39:11 -0800

    task 8 revised

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - our main function goes under here
+ * Return: 1 will show success
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return 1;
+}
