03e62fbe7b03c875afbde0c370b52714df70ed01
root <iherr.9@gmail.com>
2023-02-18 11:09:47 -0800

    task 5 fixed betty doc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - will be our function
+ * Return: will be o for success
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
