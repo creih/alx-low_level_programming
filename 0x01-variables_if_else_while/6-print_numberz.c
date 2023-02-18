ad729fb88e401cb61c10fbbbcb660a2d9b4ef0d8
root <iherr.9@gmail.com>
2023-02-18 02:22:52 -0800

    task 6 printing nbrs to 9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -print all single digit
+ * Return: will be zero if successful
+ */
+int main(void)
+{
+	int numberz;
+
+	numberz = 0;
+	while (numberz < 10)
+	{
+		putchar(48+numberz);
+		numberz++;
+	}
+	putchar('\n');
+	return 0;
+}
