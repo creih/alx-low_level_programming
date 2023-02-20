ed8a123df98e138dce621de12d8087bb304c643b
root <iherr.9@gmail.com>
2023-02-19 23:39:01 -0800

    advanced task 101 cleared to print 3digit combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main -will b where we define 3 digit combination
+ * Return: will be zero if true
+ */
+int main(void)
+{
+	int digit_0, digit_1, digit_2;
+	for (digit_0 = 0; digit_0 < 10; digit_0++)
+	{
+		for (digit_1 = digit_0; digit_1 < 10; digit_1++)
+		{
+			for (digit_2 = digit_1; digit_2 < 10; digit_2++)
+			{
+				putchar((digit_0 % 10) + 48);
+				putchar(48 + (digit_1 % 10));
+				putchar((digit_2 % 10) + 48);
+				if (digit_0 < 9)
+				{
+                               		putchar(',');
+                        	        putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
