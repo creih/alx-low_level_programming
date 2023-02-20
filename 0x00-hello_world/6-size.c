778a76054e48df1d72b626eb6cfdde19625523d9
root <iherr.9@gmail.com>
2023-02-20 01:02:12 -0800

    removed variables and used dtatype keyword instead

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,21 +5,11 @@
  */
 int main(void)
 {
-	char character;
-	
 	printf("Size of a char: %c byte(s)\n", (unsigned)sizeof(character));
-	int integer;
-	
-	printf("Size of an int: %d byte(s)\n", (unsigned)sizeof(integer));
-	long long_int;
-	
-	printf("Size of a long int: %ld byte(s)\n", (unsigned)sizeof(long_int));
-	long long longer_int;
-	
-	printf("Size of a long long int: %lld byte(s)\n", (unsigned)sizeof(longer_int));
-	float decimal;
-        
-	printf("Size of a float: %f byte(s)\n", (unsigned)sizeof(decimal));
+	printf("Size of an int: %d byte(s)\n", sizeof(int);
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %f byte(s)\n", sizeof(float));
 	return (0)
 
 }
