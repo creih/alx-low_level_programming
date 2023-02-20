8267e12dd615a99692fc6504ef3d60303f38d341
root <iherr.9@gmail.com>
2023-02-19 23:56:40 -0800

    removed white spaces and some betty signaled errors

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,6 +6,7 @@
 int main(void)
 {
 	int digit_0, digit_1, digit_2;
+
 	for (digit_0 = 0; digit_0 < 10; digit_0++)
 	{
 		for (digit_1 = digit_0 + 1; digit_1 < 10; digit_1++)
@@ -15,10 +16,10 @@ int main(void)
 				putchar((digit_0 % 10) + 48);
 				putchar(48 + (digit_1 % 10));
 				putchar((digit_2 % 10) + 48);
-				if (digit_0 < 9)
+				if (digit_0 < 7)
 				{
-                               		putchar(',');
-                        	        putchar(' ');
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
