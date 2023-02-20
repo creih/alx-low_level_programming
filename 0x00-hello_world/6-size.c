aed699b12b20a6fb807501f8af96dd1b361f53eb
root <iherr.9@gmail.com>
2023-02-20 01:17:06 -0800

    resolved betty doc problem

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main of this task is 
- * under this comments
+ * main -of this task is under this comments
+ * Return: (0)
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a char: %2d byte(s)\n", sizeof(char));
+	printf("Size of an int: %2d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %2d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %2d byte(s)\n", sizeof(float));
