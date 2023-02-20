c3fc2d40a301bcd5dd213afb7203647527a6c3b6
root <iherr.9@gmail.com>
2023-02-19 23:50:19 -0800

    fixed some logical error in the codes

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,9 +8,9 @@ int main(void)
 	int digit_0, digit_1, digit_2;
 	for (digit_0 = 0; digit_0 < 10; digit_0++)
 	{
-		for (digit_1 = digit_0; digit_1 < 10; digit_1++)
+		for (digit_1 = digit_0 + 1; digit_1 < 10; digit_1++)
 		{
-			for (digit_2 = digit_1; digit_2 < 10; digit_2++)
+			for (digit_2 = digit_1 + 1; digit_2 < 10; digit_2++)
 			{
 				putchar((digit_0 % 10) + 48);
 				putchar(48 + (digit_1 % 10));
