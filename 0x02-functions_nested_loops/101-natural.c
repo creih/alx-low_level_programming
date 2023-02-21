16f62faeb60a782bd2198a991998b5ec4f30f522
root <iherr.9@gmail.com>
2023-02-21 14:47:02 -0800

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - natural divisible by 3, 5
+ * Return: 0 for true
+ */
+int main(void)
+{
+	int num = 1, sum = 0;
+
+	while (num < 1024)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
