33b5e7d7d98437cea87dec794ff41a761eb305a1
root <iherr.9@gmail.com>
2023-02-21 14:15:32 -0800

    used llu instead of lld

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	long long c = 0, count = 0, a = 1, b = 2;
 
-	printf("%lld, %lld", a, b);
+	printf("%llu, %llu", a, b);
 	while (count < 98)
 	{
 		c = a + b;
-		printf(", %lld", c);
+		printf(", %llu", c);
 		a = b;
 		b = c;
 		count++;
