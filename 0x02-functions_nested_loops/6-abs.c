fd83571af597388d01c89e13722569cb39782a74
root <iherr.9@gmail.com>
2023-02-22 12:41:13 -0800

    task 6 even thugh I'm not sure of it

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "msin.h"
+/**
+ * _abs - calculates absolute value
+ * @: parameterto be checked
+ * Return: (0) if true
+ */
+int _abs(int)
+{
+	if (num < 0)
+	{
+		num *= -1;
+		return num;
+	}
+	else
+	{
+		retutrn num;
+	}
+}
