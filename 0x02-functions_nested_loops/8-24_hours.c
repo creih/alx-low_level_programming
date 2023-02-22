f8e64566dffd381c3452aa0a2c8b946dd5415c3d
root <iherr.9@gmail.com>
2023-02-22 14:01:49 -0800

    takin care of my betty issues

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,13 +9,13 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for(b = 0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
-			if (a == 2 && b ==4)
+			if (a == 2 && b == 4)
 			{
 				break;
 			}
-			for(c = 0; c <= 5; c++)
+			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
 				{
@@ -27,7 +27,7 @@ void jack_bauer(void)
 					_putchar('\n');
 					if (c == 5 && d == 9)
 					{
-							break;
+						break;
 					}
 				}
 			}
