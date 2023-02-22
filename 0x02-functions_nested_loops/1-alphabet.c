8ec257623e8ef79147518e378a05db86abce7752
root <iherr.9@gmail.com>
2023-02-22 00:17:39 -0800

    retrying this out

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "1-main.c"
+/**
+ * main - will call this function
+ * Return: 0 if true
+ */
+int main(void)
+{
+	char word;
+
+	for (word = 'a'; word <= 'z'; word++)
+	{
+		_putchar(word);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#ifndef "main.h"
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+#endif
