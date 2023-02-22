704c0093de7f5247d2b5a023124c00a3c4926944
root <iherr.9@gmail.com>
2023-02-22 14:18:38 -0800

    task 9 fixed my betty and fixed ascii issues

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,17 @@
 void times_table(void)
 {
 	int x, y;
-	for (x =0; x <=9; x++)
+
+	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-			_putchar(x * y);
-			_putchar(',');
-			_putchar(' ');
+			_putchar(x * y + '0');
+			if(x * y < 81)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
