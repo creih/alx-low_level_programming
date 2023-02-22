46e72cf49e7595408ac314116524b0a87b2555dc
root <iherr.9@gmail.com>
2023-02-22 13:16:53 -0800

    task 8 printing minutes of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer -to print 24 hrs
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		_putchar(a + '0');
+		for (b = 0; b <= 4; b++)
+		{
+			_putchar(b + '0');
+			_putchar(':')
+			for (c = 0; c <= 6; c++)
+			{
+				_putchar(c + '0');
+				for (d = 0; d <= 9; d++)
+				{
+					_putchar(d + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
