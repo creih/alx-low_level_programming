39731c584aa97a956889f3d3226e765dc0129d35
root <iherr.9@gmail.com>
2023-02-22 12:53:21 -0800

    task 7 should be a piece of cake

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of a number
+ * @digit: the source
+ * Return: (digit) for true
+ */
+int print_last_digit(int digit)
+{
+	digit %= 10;
+	return (digit);
+}
