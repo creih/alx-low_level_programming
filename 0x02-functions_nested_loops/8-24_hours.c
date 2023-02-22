5cc138fc86a605c51fddc639cf8ef52cc889fcb2
root <iherr.9@gmail.com>
2023-02-22 13:33:59 -0800

    used 2 variable instead of 4

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,23 +5,19 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int i, j;
 
-	for (a = 0; a <= 2; a++)
+	for (i = 0; i <= 2; i++)
 	{
-		for (b = 0; b <= 4; b++)
+		for (j = 0; j <= 9; j++)
 		{
-			for (c = 0; c <= 6; c++)
+			_putchar(i + '0');
+			_putchar(':');
+			_putchar(j + '0');
+			_putchar('\n');
+			if (j == 60)
 			{
-				for (d = 0; d <= 9; d++)
-				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-				}
+				break;
 			}
 		}
 	}
