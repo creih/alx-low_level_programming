e27f88903e6f3afe3dfe12b430e02af97f954719
root <iherr.9@gmail.com>
2023-02-23 11:48:06 -0800

    task 2-fixxed betty

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a >= b && a >= c)
-    {
-        largest = a;
-    }
-    else if (b >= a && b >= c)
-    {
-        largest = b;
-    }
-    else if (c >= b && c >= b)
-    {
-        largest = c;
-    }
-    else
-    {
-	    return 0;
-    }
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else if (c >= b && c >= b)
+	{
+		largest = c;
+	}
+	else
+	{
+		return (0);
+	}
 
-    return (largest);
+	return (largest);
 }
