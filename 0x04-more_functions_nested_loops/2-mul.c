0e78c3067faf71c04dfe9b23e3ff56a6ad806ec4
root <iherr.9@gmail.com>
2023-02-23 13:18:24 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - product of 2 nums
+ * @a: first num
+ * @b: second num
+ * Return: 0 always
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	_putchar(product + '0');
+	return (0);
+}
