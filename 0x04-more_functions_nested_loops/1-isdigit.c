504f75a9c116485725f605441fa24a968e77ac32
root <iherr.9@gmail.com>
2023-02-23 13:08:11 -0800

    adding task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - will check if c is num
+ * @c: will becheked
+ * Return: (1)true, (0)otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
