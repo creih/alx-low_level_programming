4aefef9de2eeb697cce8c7de3a1176e8ccac0f98
root <iherr.9@gmail.com>
2023-02-24 04:12:45 -0800

    fixed main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,4 @@
-#ifndef MAIN_H _pUTCHAR_H ISUPPER_H ISDIGIT_H MUL_H
-#define MAIN_H _pUTCHAR_H ISUPPER_H ISDIGIT_H MUL_H
 int _putchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-#endif
