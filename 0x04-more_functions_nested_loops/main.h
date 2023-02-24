7c3bf2da1f06dfe303e7aa0e04c4d2111d59f45c
root <iherr.9@gmail.com>
2023-02-24 04:00:02 -0800

    changed if statement

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,7 @@
+#ifndef MAIN_H _pUTCHAR_H ISUPPER_H ISDIGIT_H MUL_H
+#define MAIN_H _pUTCHAR_H ISUPPER_H ISDIGIT_H MUL_H
 int _putchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+#endif
