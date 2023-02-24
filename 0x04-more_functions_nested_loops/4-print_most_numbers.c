52043e5695b906568b573ba00a8a5dbc6a894895
root <iherr.9@gmail.com>
2023-02-24 14:12:42 -0800

    trying task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - except 2 nad 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+	{
+		if (num == 2 || num == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(num);
+		}
+	}
+}
