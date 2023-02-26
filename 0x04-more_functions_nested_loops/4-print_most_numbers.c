0fc84801ff161de18a2f1937146a88eb5ad885dd
root <iherr.9@gmail.com>
2023-02-26 11:27:49 -0800

    removed 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,13 @@ void print_most_numbers(void)
 
 	for (num = 0; num < 10; num++)
 	{
-		if (num != 2 || num != 4)
+		if (num == 2 || num == 4)
 		{
-			_putchar(num + '0');
+			continue;
+		}
+		else
+		{
+			_putchar(num + '0);
 		}
 	}
 	_putchar('\n');
