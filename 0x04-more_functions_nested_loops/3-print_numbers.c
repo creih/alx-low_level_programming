e855cecf94310007f5712f1217b4d8461958ab96
root <iherr.9@gmail.com>
2023-02-26 11:58:51 -0800

    re-written everything

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - for printin nums
+ * Return: none
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
