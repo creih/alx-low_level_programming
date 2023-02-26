aec916c319c5d5814ad696bef9b8de320e7bb1dc
root <iherr.9@gmail.com>
2023-02-26 13:46:47 -0800

    just did thi

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - to print a line
+ * @n: nbr of time to print _
+ * Return: return nothing
+ */
+void print_lines(int n)
+{
+	int nbr;
+
+	for (nbr = 0; nbr <= n; nbr++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
