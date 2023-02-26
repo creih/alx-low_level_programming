5794e659e25deb4d95c0eda82b30cdc99f540195
root <iherr.9@gmail.com>
2023-02-26 11:16:18 -0800

    did fix betty for sure

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,13 @@
 void print_most_numbers(void)
 {
 	int num;
+
 	for (num = 0; num < 10; num++)
 	{
-		if (num == 2 || num == 4)
-		{
-			continue;
-		}
-		else
+		if (num != 2 || num != 4)
 		{
 			_putchar(num);
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
