de716d95f952fcfe0338e973c4055e729398e18e
root <iherr.9@gmail.com>
2023-02-26 10:20:44 -0800

    did fizzbuzz

---
+++ 0x04-more_functions_nested_loops/0-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - fizzbuzz
+ * Return: (0 return)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	return (0);
+}
