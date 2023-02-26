00259880b3863a9ed25e34a11c5b21109c841142
root <iherr.9@gmail.com>
2023-02-26 12:10:24 -0800

    printing square nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - for printing square made of nums
+ * Return: none
+ */
+void more_numbers(void)
+{
+	int line, numbers;
+
+	for (line = 0 ;line <= 10; line++)
+	{
+		for (numbers = 0; numbers<=14; numbers++)
+		{
+			_putchar(numbers + '0');
+		}
+	}
+}
