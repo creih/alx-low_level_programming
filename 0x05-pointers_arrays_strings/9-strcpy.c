c8f5c8984adcef12e2622c48bc04ca3ff9264d77
creih <iherr.9@gmail.com>
2023-02-27 23:31:38 +0200

    copying with task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.c"
+/**
+ * *_strcpy -copies the pointer dest
+ * @dest: final place
+ * @src: source
+ * Return: dest if true
+ */
+char *_strcpy(char *dest, char * src)
+{
+	dest = src;
+	return (*dest);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ int _putchar(char c);
 void print_rev(char *s);
 void print_array(int *a, int n);
 void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
