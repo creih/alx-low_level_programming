070dca063042e0f5eea27e3732603bb8796d6e09
creih <iherr.9@gmail.com>
2023-02-27 21:17:34 +0200

    just giving it my first shot

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - will print from backwards
+ * @s: will be our parameter
+ * Return: (none)
+ */
+void print_rev(char *s)
+{
+	while (*s)
+	{
+		s++;
+	}
+	while (*s)
+	{
+		_putchar(s);
+		s--;
+	}
+}
