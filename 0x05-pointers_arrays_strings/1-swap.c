344e2da0b7572611065b221d8d1f5c88db96f729
root <iherr.9@gmail.com>
2023-02-27 01:24:31 -0800

    well did the firt=st trial of task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int- will swap two numbers
+ * @a: first param
+ * @b: second param
+ * Return: nothing as its void
+ */
+void swap_int(int *a, int *b)
+{
+	int *c;
+	*c = a;
+	*a = b;
+	*b = a;
+}
