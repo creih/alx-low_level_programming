1f4e17fcd3904bdba1e23b8358df46ac1d826f22
root <iherr.9@gmail.com>
2023-02-27 00:34:10 -0800

    did the 0 task for first run

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98- reverts the variable to 98
+ * @n: pointer to variale
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	int num;
+
+	*n = &num;
+	*n = 98;
+}
