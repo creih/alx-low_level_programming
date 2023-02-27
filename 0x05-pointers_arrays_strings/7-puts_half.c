9ae9cbfb09671f9d01269cebe08da86e8a2d1c79
creih <iherr.9@gmail.com>
2023-02-27 23:09:08 +0200

    7 task here we go

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts_half - print half the string
+ * @str: parameter
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int size = _strlen(str), i;
+
+	for (i = size / 2; i <= size; size++)
+	{
+		printf("%d", str[i]);
+	}
+}
