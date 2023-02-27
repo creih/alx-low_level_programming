0455c629b26e36437fea405a32152a3add83f703
creih <iherr.9@gmail.com>
2023-02-27 22:40:34 +0200

    ended task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_array -print array elements
+ * @n: nbr of elements of array
+ * @*a: array name
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(*a + '0');
+	}
+}
