dd09848960900e6b42ada8013db6dd416eef20ae
creih <iherr.9@gmail.com>
2023-02-27 20:28:11 +0200

    used while to loop through the whole string and putchar to print them characters

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - will print all chrs of the string
+ * @*s: will be the parameter of file
+ * Return: none
+ */
+void _puts(char *str)
+{
+	while (str++)
+	{
+		_putchar(str);
+	}
+	_putchar('\n');
+}
