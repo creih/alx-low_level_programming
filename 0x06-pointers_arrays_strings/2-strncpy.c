a47015358335259f2140afa63a4513ce331a7f34
creih <iherr.9@gmail.com>
2023-03-02 22:34:31 +0200

    task 2 trial#0

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - to copy string
+ * @dest: 2nd pointer
+ * @src: 1st pointer
+ * @n: index to start cp from
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; index < n && src[index] != '\0'; index++)
+	{
+		dest[index] = src [index];
+	}
+	for (; index < n; index++)
+	{
+		dest[index] = '\0';
+	}
+	return (dest);
+}
