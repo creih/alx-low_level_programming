3818c263ea4797c2c2810fecf0b7322a849e3bcc
creih <iherr.9@gmail.com>
2023-03-03 00:25:42 +0200

    task6 trial#0

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ * cap_string - make 1st string cap
+ * @c: param pointer
+ * Return: none
+ */
+char *cap_string(char *c)
+{
+	int y;
+
+	if (c[0] >= 'a' && c[0] <= 'z')
+	{
+		c[0] = c[0] - ('a' - 'A');
+	}
+	for (y = 1; c[y] != '\0'; y++)
+	{
+		if (c[y - 1] == ' ' || c[y -1] == '\t')
+		{
+			if (c[y] >= 'a' && c[y] <= 'z')
+			{
+				c[y] = c[y] - ('a' - 'A');
+			}
+		}
+		if (c[y - 1] == '\n' || c[y - 1] == ',')
+		{
+			if (c[y] >= 'a' && c[y] <= 'z')
+			{
+				c[y] = c[y] - ('a' - 'A');
+			}
+		}
+		if (c[y - 1] == ';' || c[y - 1] == '.')
+		{
+			if (c[y] >= 'a' && c[y] <= 'z')
+			{
+				c[y] = c[y] - ('a' - 'A' );
+			}
+		}
+		if (c[y - 1] == '!' || c[y - 1] == '?')
+		{
+			if (c[y] >= 'a' && c[y] <= 'z')
+			{
+				c[y] = c[y] - ('a' - 'A');
+			}
+		}
+		if (c[y - 1] == '"' ||  c[y - 1] == '(')
+		{
+			if (c[y] >= 'a' && c[y] <= 'z')
+			{
+				c[y] = c[y] - ('a' - 'A');
+			}
+		}
+		if (c[y -1] == ')')
+		{
+			if (c[y] >= 'a' && c[y] <= 'z')
+			{
+				c[y] = c[y] - ('a' - 'A');
+			}
+		}
+	}
+	return (c);
+}
