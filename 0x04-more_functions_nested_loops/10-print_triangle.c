7ccd61a3410fcb74e5676b401a52ec024a50a18b
creih <iherr.9@gmail.com>
2023-03-04 20:32:51 +0200

    done with task10 trial#1

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle -right 90_def angle
+ * @size: parameter
+ */
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (n = 0; n < size - 1; n++)
+			{
+				_putchar(' ');
+			}
+			for (y = 0; y <= x; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
