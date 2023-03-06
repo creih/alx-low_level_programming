5087a3c0e73db611853201d0fafcabd081fe6623
creih <iherr.9@gmail.com>
2023-03-06 23:05:32 +0200

    taask3 trial0

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn -to check prefix present
+ * @s: initial segment
+ * @accept: other segment
+ * Return: nbr of success
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int n_1, n_2;
+	int success = 0;
+
+	for (n_1 = 0; s[n_1] != '\0'; n_1++)
+	{
+		for (n_2 = 0; accept[n_2] != '\0'; n_2++)
+		{
+			if (accept[n_2] == s[n_1])
+			{
+				success++;
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+	return (success);
+}
