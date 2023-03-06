2f09b35f6585b6ec4f1a023bf9df64323561a820
creih <iherr.9@gmail.com>
2023-03-07 00:27:54 +0200

    task 6 trial01

---
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_chessboard -printing characters of chess
+ * @a: pointer
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x;
+
+	for (x= 0; x <= 7; x++)
+	{
+		_putchar(*a);
+		_putchar('\n');
+	}
+}
