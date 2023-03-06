4a93bcb56376efed9502150b349730d0272a00cb
creih <iherr.9@gmail.com>
2023-03-06 22:41:21 +0200

    task2 trial1

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strchr -locate a char
+ * @s: pointer to sth
+ * @c: char to locate
+ * Return: address of c else null
+ */
+char *_strchr(char *s, char c)
+{
+	int n;
+	char *found;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		if (*s == c)
+		{
+			found = s;
+			break;
+		}
+		else
+		{
+			found = '\0';
+		}
+	}
+	return (found);
+}
