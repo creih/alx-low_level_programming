b988a503ab27488559fa8b8f72b598154a943f86
creih <iherr.9@gmail.com>
2023-03-07 21:51:29 +0200

    task 5 trial1

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,5 +7,18 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	
+	int x, y, j;
+
+	for (j = 0; needle[j] = '\0'; j++)
+	{
+		for (i = 0; haystack[i] = '\0'; i++)
+		{
+			if (needle[j] == haystack[i])
+			{
+				break;
+			}
+		}
+		return (haystack[i]);
+	}
+	return ('\0');
 }
