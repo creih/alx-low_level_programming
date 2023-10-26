1f55eb74755d2f80755816c42a62ad53cf33dd26
creih <iherr.9@gmail.com>
2023-10-26 21:36:12 +0200

    fixed my function descriptions

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,11 +9,15 @@
  */
 int is_palindrome_recursive(char *s, int start, int end)
 {
-    if (start >= end)
-        return (1);
-    if (s[start] != s[end])
-        return (0);
-    return is_palindrome_recursive(s, start + 1, end - 1);
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+	return is_palindrome_recursive(s, start + 1, end - 1);
 }
 /**
  * is_palindrome - to check and call the latter one
@@ -23,6 +27,6 @@ int is_palindrome_recursive(char *s, int start, int end)
 
 int is_palindrome(char *s)
 {
-    int length = strlen(s);
-    return is_palindrome_recursive(s, 0, length - 1);
+	int length = strlen(s);
+	return is_palindrome_recursive(s, 0, length - 1);
 }
