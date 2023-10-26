e63fb1de4ca3e3a78cd6aeedc2f8e8cd0984789f
creih <iherr.9@gmail.com>
2023-10-26 22:15:29 +0200

    trying to fix earlier bugs

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,46 +1,50 @@
 #include "main.h"
 #include <math.h>
 /**
- * _sqrt_recursion - sqrt of the number
+ * _sqrt_recursive - sqrt of the number
  * @n: nbr passed
+ * @low: the least
+ * @high: the upper
  * Return: square root of n
  */
 #include <stdio.h>
 
 int _sqrt_recursive(int n, int low, int high)
 {
-    if (low > high)
-    {
-        return -1; // Number doesn't have a natural square root
-    }
-
-    int mid = (low + high) / 2;
-    int mid_square = mid * mid;
-
-    if (mid_square == n)
-    {
-        return mid; // Found the square root
-    }
-    else if (mid_square > n)
-    {
-        return _sqrt_recursive(n, low, mid - 1); // Search in the lower half
-    }
-    else
-    {
-        return _sqrt_recursive(n, mid + 1, high); // Search in the upper half
-    }
+	int mid = (low + high) / 2;
+	int mid_square = mid * mid;
+	if (low > high)
+	{
+		return (-1);
+	}
+	if (mid_square == n)
+	{
+		return (mid);
+	}
+	else if (mid_square > n)
+	{
+		return (_sqrt_recursive(n, low, mid - 1));
+	}
+	else
+	{
+		return (_sqrt_recursive(n, mid + 1, high));
+	}
 }
-
+/**
+ * _sqrt_recursion - calls the upper defined function
+ * @n: the number i'd be using
+ * Return: some nbr
+ */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return -1; // Negative numbers don't have a natural square root
-    }
-    if (n == 0 || n == 1)
-    {
-        return n; // 0 and 1 are their own square roots
-    }
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
 
-    return _sqrt_recursive(n, 0, n);
+	return (_sqrt_recursive(n, 0, n));
 }
