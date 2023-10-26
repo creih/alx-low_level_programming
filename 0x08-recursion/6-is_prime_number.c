9f7d3e0d5255ac05c0b4e2cc52430196599e0b11
creih <iherr.9@gmail.com>
2023-10-26 21:48:12 +0200

    fixed the issue with 1 as input

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,17 +6,22 @@
  */
 int is_prime_number(int n)
 {
-	if (n == 1)
-	{
-		return (1);
-	}
-	else if (n <= 0)
+	int i;
+
+	if (n <= 1)
 	{
 		return (0);
 	}
-	else
+	if (n == 2)
 	{
-		return (n / is_prime_number(n - 1));
+		return (1);
 	}
-	return (0);
+	for (int i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
