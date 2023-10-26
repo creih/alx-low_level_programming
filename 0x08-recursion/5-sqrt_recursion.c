dd2fdd3e2aaa2f1af92e0289f2c27b8917bb48bd
creih <iherr.9@gmail.com>
2023-10-26 22:04:30 +0200

    trying to fix earlier bugs

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,28 +5,42 @@
  * @n: nbr passed
  * Return: square root of n
  */
+#include <stdio.h>
+
+int _sqrt_recursive(int n, int low, int high)
+{
+    if (low > high)
+    {
+        return -1; // Number doesn't have a natural square root
+    }
+
+    int mid = (low + high) / 2;
+    int mid_square = mid * mid;
+
+    if (mid_square == n)
+    {
+        return mid; // Found the square root
+    }
+    else if (mid_square > n)
+    {
+        return _sqrt_recursive(n, low, mid - 1); // Search in the lower half
+    }
+    else
+    {
+        return _sqrt_recursive(n, mid + 1, high); // Search in the upper half
+    }
+}
+
 int _sqrt_recursion(int n)
 {
-	int mid, high = n, low = 0;
+    if (n < 0)
+    {
+        return -1; // Negative numbers don't have a natural square root
+    }
+    if (n == 0 || n == 1)
+    {
+        return n; // 0 and 1 are their own square roots
+    }
 
-	if (high >= low)
-	{
-		mid = (high + low) / 2;
-		if ((mid * mid <= n) && ((mid + 1) + (mid + 1) > n))
-		{
-			return (mid);
-		}
-		else if (mid * mid < n)
-		{
-			return (_sqrt_recursion(mid + 1));
-		}
-		else
-		{
-			return (_sqrt_recursion(mid - 1));
-		}
-	}
-	else
-	{
-		return (-1);
-	}
+    return _sqrt_recursive(n, 0, n);
 }
