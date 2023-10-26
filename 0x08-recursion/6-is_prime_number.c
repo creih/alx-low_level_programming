12051e508177f6813808f0b5ad971e934dc1e676
creih <iherr.9@gmail.com>
2023-10-26 16:11:30 +0200

    7th task though 6th was horrible

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,9 +10,13 @@ int is_prime_number(int n)
 	{
 		return (1);
 	}
+	else if (n < 0)
+	{
+		return (n / isprime_number(-n - 1)
+	}
 	else
 	{
-		return (n % is_prime_number(n - 1));
+		return (n / is_prime_number(n - 1));
 	}
 	return (0);
 }
