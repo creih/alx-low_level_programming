5e6168e2ec9e783367280d98130fbeb12bc39dad
creih <iherr.9@gmail.com>
2023-10-26 20:38:39 +0200

    juggling betty

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 /*
  *wildcmp_recursive - to compare 2 strings
+ * @s1 string 1
+ * @s2 string 2
  * Return: will be 0.
  */
+
 int wildcmp_recursive(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-        return 1;
-    if (*s2 == '*')
-    {
-        return wildcmp_recursive(s1, s2 + 1) || wildcmp_recursive(s1 + 1, s2);
-    }
-    if (*s1 == *s2 || *s2 == '?')
-        return wildcmp_recursive(s1 + 1, s2 + 1);
-    return 0;
+	if (*s1 == '\0' && *s2 == '\0')
+		return 1;
+	if (*s2 == '*')
+	{
+		return wildcmp_recursive(s1, s2 + 1) || wildcmp_recursive(s1 + 1, s2);
+	}
+	if (*s1 == *s2 || *s2 == '?')
+	{
+		return wildcmp_recursive(s1 + 1, s2 + 1);
+	}
+	return 0;
 }
 /*
  * wildcmp - calls the latter function simply
- * *s1 string 1
- * *s2 string 2
+ * @s1 string 1
+ * @s2 string 2
  * Return: hopefully 0.
  */
+
 int wildcmp(char *s1, char *s2)
 {
-    return wildcmp_recursive(s1, s2);
+	return wildcmp_recursive(s1, s2);
 }
