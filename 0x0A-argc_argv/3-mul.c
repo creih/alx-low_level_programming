c9e1134e413de53c2989655efe58c8942f8c94ef
creih <iherr.9@gmail.com>
2023-10-28 11:32:51 +0200

    substituted for with while

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+/**
+ * main - multiplicate func main
+ * @argv: 1st argument
+ * @argc: 2nd argument
+ * Return: 0 for success
+ */
 int main(int argc, char *argv[])
 {
-	for (int i = 0; i < argc; i++) {
-		printf("%s\n", argv[i]);
-	}
-	return 0;
-}
+	int i = 0;
 
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
