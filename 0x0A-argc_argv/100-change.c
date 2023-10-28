2866984fd8abcba2d736b4e5cc62a4e7a5d2b7d5
creih <iherr.9@gmail.com>
2023-10-28 11:22:51 +0200

    removed some white space

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,29 +8,36 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents = atoi(argv[1]);
+	int cents, num_coins, count;
 	int coins[] = {25, 10, 5, 2, 1};
-	int num_coins = sizeof(coins) / sizeof(coins[0]);
-	int count = 0;
+	int i = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		return 1;
 	}
+	cents = atoi(argv[1]);
+	num_coins = sizeof(coins) / sizeof(coins[0]);
+	count = 0;
 	if (cents < 0)
 	{
 		printf("0\n");
-		return (0);
+		return 0;
 	}
-	for (int i = 0; i < num_coins; i++)
+	while (i < num_coins)
 	{
-		while (cents >= coins[i])
+		if (cents >= coins[i])
 		{
 			cents -= coins[i];
 			count++;
 		}
+		else
+		{
+			i++;
+		}
 	}
 	printf("%d\n", count);
-	return (0);
+	return 0;
 }
+
