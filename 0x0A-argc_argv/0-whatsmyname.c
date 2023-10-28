7809239367f67127a6bcae17bd88331901b0bd08
creih <iherr.9@gmail.com>
2023-10-28 10:38:23 +0200

    finally approved and checked by betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - function for task
+ * @argc: 1st arg of main
+ * @argv: 2nd arg
+ * Return: 0 for success
+ */
 int main(int argc, char *argv[])
 {
     (void)argc;
