38f29491db95fba49a142f67fb3c5f1f3bbab08e
creih <iherr.9@gmail.com>
2023-10-28 08:52:27 +0200

    task 4 correction of betty's suggestion

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,43 +9,12 @@
  */
 int is_positive_number(const char *str)
 {
-    while (*str)
-    {
-        if (!isdigit(*str))
-            return (0);
-        str++;
-    }
-    return (1);
+	while (*str)
+	{
+	if (!isdigit(*str))
+		return (0);
+		str++;
+	}
+	return (1);
 }
 
-/**
- * main - Entry point of the program
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- * Return: 0 if successful, 1 if there are non-numeric arguments
- */
-int main(int argc, char *argv[])
-{
-    int i, sum = 0;
-
-    if (argc < 2)
-    {
-        printf("0\n");
-        return (0);
-    }
-
-    for (i = 1; i < argc; i++)
-    {
-        if (!is_positive_number(argv[i]))
-        {
-            printf("Error\n");
-            return (1);
-        }
-
-        sum += atoi(argv[i]);
-    }
-
-    printf("%d\n", sum);
-
-    return (0);
-}
