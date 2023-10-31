137c052fdf10ee8cc09dbe45166961ad27575e93
creih <iherr.9@gmail.com>
2023-10-31 22:38:26 +0100

    fixed soe betties

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -5,8 +5,10 @@
  * @height: how many columns in grid
  * Return: void
  */
-void free_grid(int **grid, int height) {
+void free_grid(int **grid, int height)
+{
 	int i = 0;
+
 	if (grid != NULL)
 	{
 		while (i < height)
