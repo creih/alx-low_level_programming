d0ab2b061a3ade50269e247e772278adda433901
creih <iherr.9@gmail.com>
2023-10-31 21:43:41 +0100

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include<stdlib.h>
+/**
+ * alloc_grid - allocate memory fr 2dim array
+ * @width: dim1 of array
+ * @height: dim2 of array
+ * Return: pointer to str if successfull els NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int *num, x, y;
+
+	if (height == 0 || width == 0)
+	{
+		return 0;
+	}
+	if (width < 0 || height < 0)
+	{
+		return NULL;
+	}
+	num = malloc(sizeof(int)*width*height);
+	if (num == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		while (y < height)
+		{
+			while (x < width)
+			{
+				num[y][x] = 0;
+			}
+		}
+	}
+	return (num);
+}
