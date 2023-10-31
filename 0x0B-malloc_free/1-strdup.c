4fe5b9f07185db2f9a7d12ab65ee2b527e7e19c5
creih <iherr.9@gmail.com>
2023-10-31 19:53:27 +0100

    added the null cha at end of dup string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,21 +9,22 @@ char *_strdup(char *str)
 	int i = 0, size = 0;
 	char *dup;
 
+	if (str == NULL)
+        {
+                return (NULL);
+        }
 	while (str[i] != '\0')
 	{
 		size++;
 		i++;
 	}
-	if (str == NULL)
-	{
-		return (NULL);
-	}
 	i = 0;
-	dup = malloc(sizeof(char) * size);
+	dup = malloc(sizeof(char) * size + 1);
 	while (i < size)
 	{
 		dup[i] = str[i];
 		i++;
 	}
+	dup[size] = '\0';
 	return (dup);
 }
