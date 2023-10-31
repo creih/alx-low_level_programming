84c94e6b872cb19abee7de224e52c48979bbfa2a
creih <iherr.9@gmail.com>
2023-10-31 08:56:41 +0100

    changed if argument

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,14 @@ char *create_array(unsigned int size, char c)
 	char *d;
 	unsigned int i = 0;
 
-	if (size != 0)
+	if (size > 0)
 	{
-		d = malloc(sizeof(char) * size);
+		d = malloc(sizeof(c) * size);
 		while (i < size)
 		{
 			d[i] = c;
+			printf("%s", d[i]);
 		}
-		return (d);
 	}
 	else
 	{
