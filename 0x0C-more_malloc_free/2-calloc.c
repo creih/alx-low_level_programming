09d30a1982a4c5f3fc6b5f771f4f38cdc916b33c
creih <iherr.9@gmail.com>
2023-11-02 22:01:50 +0200

    adding  file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,30 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _calloc - array mem loc
- * @nmemb: elements
- * @size: how long ne is
- * Return: void
+ * _calloc - Allocates memory for an array using malloc
+ * @nmemb: Number of elements in the array
+ * @size: Size of each element in bytes
+ * Return: Pointer to the allocated memory or NULL if malloc fails
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	size_t i = 0, total_size;
-	char *ptr;
+	void *ptr;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	total_size = nmemb * size;
-	ptr = malloc(total_size);
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	while (i < total_size)
-	{
-		ptr[i] = 0;
-		i++;
-	}
-	return;
+	return (ptr);
 }
