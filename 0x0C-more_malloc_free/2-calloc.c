6ce4d974ef254a952b7dce10d08bcfa0dafc612e
creih <iherr.9@gmail.com>
2023-11-02 21:16:15 +0200

    added task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+/**
+ * _calloc - allocate memory for array
+ * @nmemb: elements of array
+ * @size: size of array
+ * Return: pointer to memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *point = malloc(size * nmemb);
+	if (point == NULL)
+	{
+		return (NULL);
+	}
+	if (nmemb == 0 || size == 0)
+	{
+		retun (NULL);
+	}
+	return (point);
+}
