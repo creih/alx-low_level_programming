ef50d71c7f31d44802f0bbe6f1ef92ccd2286e17
creih <iherr.9@gmail.com>
2023-11-02 22:22:41 +0200

    resolved betty issues

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * _realloc - Reallocates a memory block using malloc and free
  * @ptr: Pointer to the memory previously allocated with malloc
@@ -11,8 +10,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    void *new_ptr;
-    unsigned int i = 0;
+	void *new_ptr;
+	unsigned int i = 0;
 
 	if (new_size == old_size)
 	{
@@ -41,7 +40,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		new_size = old_size;
 	}
-	while(i < new_size)
+	while (i < new_size)
 	{
 		*((char *)new_ptr + i) = *((char *)ptr + i);
 		i++;
