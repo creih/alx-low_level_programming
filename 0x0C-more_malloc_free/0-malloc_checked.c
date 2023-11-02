c892c2613b3daa0dec4ad7128a2a82fb3c3e8dda
creih <iherr.9@gmail.com>
2023-11-02 12:11:12 +0200

    first submission on 0x0c-malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: this is the variable we'll create memory for
+ * Return: 98 on fail, else pointer to the memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *point = malloc(b);
+
+	if (point != NULL)
+	{
+		return (point);
+	}
+	else
+	{
+		exit(98);
+	}
+
+}
