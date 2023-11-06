e7e543890b87b820edc304acf06c7addcc5edbce
creih <iherr.9@gmail.com>
2023-11-06 22:41:22 +0200

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - print watever is initialised in struct vars
+ * @d: pointer to struct vars
+ * Return: NULL
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	printf("Name: %s\n", (d->name) ? d->name : "nil");
+	printf("Age: %f\n", (d->age) ? d->age : "nil");
+	printf("Name: %s\n", (d->owner) ? d->owner : "nil");
+}
