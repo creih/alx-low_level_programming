8eef4e0d8ed40f783e85a4d073acb6f15fa5a06a
creih <iherr.9@gmail.com>
2023-11-06 21:46:25 +0200

    added * to d

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,9 @@
  * @owner: owner of dog by func call
  * Return: NULL
  */
-void init_dog(struct dog d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = {name, age, owner};
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
