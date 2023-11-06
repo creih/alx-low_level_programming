a9b4475ada99e2e0c606f0f2de305f36f00355be
creih <iherr.9@gmail.com>
2023-11-06 21:41:02 +0200

    task 1answer

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+/**
+ * init_dog - initialising the dog structure
+ * @d: object of dog struct
+ * @name: name given by func call
+ * @age: age of dog by func call
+ * @owner: owner of dog by func call
+ * Return: NULL
+ */
+void init_dog(struct dog d, char *name, float age, char *owner)
+{
+	d = {name, age, owner};
+}
