658a370045862381d7b77e82480193697d48a0d5
creih <iherr.9@gmail.com>
2023-11-06 22:51:47 +0200

    added newline after displaying age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 		return;
 	printf("Name: %s\n", (d->name) ? d->name : "nil");
 	if (d->age)
-		printf("Age: %f", d->age);
+		printf("Age: %f\n", d->age);
 	else
 		printf("Age: nil\n");
 	printf("Owner: %s\n", (d->owner) ? d->owner : "nil");
