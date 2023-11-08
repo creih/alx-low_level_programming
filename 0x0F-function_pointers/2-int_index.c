25ec56a66d4bc781e70915f94d994daaef071cda
creih <iherr.9@gmail.com>
2023-11-08 21:22:42 +0200

    task 2 submission

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,12 +9,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
+	if (!array || size = 0 || !cmp)
+	{
+		return (-1);
+	}
 	while (i < size)
 	{
 		if (cmp(array[i]))
 		{
-			return i;
+			return (i);
 		}
 	}
-	return -1;
+	return (-1);
 }
