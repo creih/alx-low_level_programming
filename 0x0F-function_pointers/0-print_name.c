26fe0f23fb7ebf278c0e5625d0c043c40e1a65a7
creih <iherr.9@gmail.com>
2023-11-08 15:31:41 +0200

    task0 done and running hopefuly it checks

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/*
+ *print_name - decides wether to print uppercase or as_is
+ * @name: points to name
+ * @f: points to chosen function
+ * Return: NULL
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+void print_name(char *name, void (*f)(char *));
+#endif
