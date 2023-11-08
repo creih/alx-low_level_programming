2b0e0010d2fbde1614e4b30595c75b1d5bfc79dd
creih <iherr.9@gmail.com>
2023-11-08 16:21:33 +0200

    task1 up and runing

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+/**
+ * array_iterator - iterates through all items of array
+ * @array: array to iterate through
+ * @size: size of the array
+ * @action: func pointer
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
