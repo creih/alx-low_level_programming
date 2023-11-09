6751e6ddd84777e5539eadd77e1447e7c232dcaa
creih <iherr.9@gmail.com>
2023-11-09 16:20:51 +0200

    task 1 uploaded

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - print arg ints separted by separator
+ * @separator: pointer to char to separate the ints displayed
+ * @n: number of args
+ * Return: ints or NULL
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0, digit;
+	va_list ints;
+
+	va_start(ints, n);
+	if (separator == NULL)
+	{
+		return;
+	}
+	while (i < n)
+	{
+		digit = va_arg(ints, int);
+		printf("%d%s", digit, separator);
+		i++;
+	}
+	va_end(ints);
+	 
+}
