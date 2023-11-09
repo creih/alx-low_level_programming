014fa6b3e4f0a75ca6cae8aea9fca1b65c087020
creih <iherr.9@gmail.com>
2023-11-09 17:47:39 +0200

    trying removing ints is null condition

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ints;
 
 	va_start(ints, n);
-	if (ints == NULL || separator == NULL || n == 0)
+	if (separator == NULL || n == 0)
 	{
 		return;
 	}
