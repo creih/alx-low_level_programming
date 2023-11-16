e68869b2bac908b05cacf43c3d7b958e5feb1da5
creih <iherr.9@gmail.com>
2023-11-17 00:05:50 +0300

    task3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * add_node_end - add node at list end
+ * @head: pointer to beginning
+ * @str: str to duplicate
+ * Return: address of newelementa
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *last;
+
+	if (head == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+	new = malloc(size_of(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	if(new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = strlen(str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		last =*head;
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+		last->next = new;
+	}
+	return (new);
+}
