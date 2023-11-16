0596269e1717f0c58e276bf7ce3e157190f217a9
creih <iherr.9@gmail.com>
2023-11-16 23:17:18 +0300

    adding modified lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
+#include <stdio.h>
 int _putchar(char c);
+/**
+ * struct list_s - single list structure
+ * @str: allocated syrng memory
+ * @len: length of made string
+ * @next: points to following node
+ * Description: node structure for single linked list
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
 #endif
