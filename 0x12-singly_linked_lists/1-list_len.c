0ac11a42c19a759e88fe799e406006e7d3f5c7f1
creih <iherr.9@gmail.com>
2023-11-16 23:27:23 +0300

    taske 2

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - return elements counter
+ * @h: pointer to head
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		mode_count++;
+	}
+	return (node_count);
+}
