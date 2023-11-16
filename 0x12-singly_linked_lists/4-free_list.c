7658107eba52a07d3e59c043180dc89ecb504bb7
creih <iherr.9@gmail.com>
2023-11-17 00:23:05 +0300

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * free_list - frees as the name suggests
+ * @head: pointer to start
+ * Return: nil
+ */
+void free_list(list_t *head)
+{
+	list_t *curr;
+
+	while (head != NULL)
+	{
+		curr = head;
+		head = head->next;
+		free(curr->str);
+		free(curr);
+	}
+}
