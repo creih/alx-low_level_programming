5efd1b98f09045025150375be42d544184360fbd
eric0149 <eriscorplock@gmail.com>
2023-11-21 17:31:56 +0300

    task 1 done and ready

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * listint_len - counts linked list nodes
+ * @h: the ptr to the list 
+ * Return: 0 for fail nbr of nodes on success
+ */
+size_t listint_len(const listint_t *h)
+{
+	int counter = 0;
+
+	if (h)
+	{
+		const listint_t *temp;
+
+		temp = h;
+		while (temp)
+		{
+			counter++;
+			temp = temp->next;
+		}
+		return (counter);
+	}
+	return (0);
+}
