67e972302227614b8048e9bf8c5a71f8ec17dfa6
eric0149 <eriscorplock@gmail.com>
2023-11-21 21:49:33 +0300

    task 5 done ready for check

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - stes head to null
+ * @head: pointer to list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
