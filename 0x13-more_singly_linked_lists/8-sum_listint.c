2d2104eb1d62242d268a2651e92be0135b1677f5
eric0149 <eriscorplock@gmail.com>
2023-11-22 23:17:54 +0300

    task 8 done

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *temp;
+	int igiteranyo = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	temp = head;
+	while (temp != NULL)
+	{
+		igiterany += temp->n;
+		temp = temp->next;
+	}
+	return (igiteranyo);
+}
