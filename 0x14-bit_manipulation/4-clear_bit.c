6bec950e71f75268a32b14a536b8a17b2e2c6eea
eric0149 <eriscorplock@gmail.com>
2023-11-23 22:58:09 +0300

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * clear_bit - set value at index to 0
+ * @n: nbr
+ * @index: index to chane at
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	*n = *n & ~(1 << index);
+	return (1);
+}
