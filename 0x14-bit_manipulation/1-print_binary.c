50377caa0687069252f7b099dc31519ab98a11aa
eric0149 <eriscorplock@gmail.com>
2023-11-23 21:59:19 +0300

    task 1 runnin

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_binary - print binary nbr of n
+ * @n: number to turn into binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int bar; 
+	int bara = sizeof(n) * 8 - 1;
+
+	while (bara >= 0)
+	{
+		bar = (n >> bara) & 1;
+		_putchar(bar);
+		bara--;
+	}
+}
