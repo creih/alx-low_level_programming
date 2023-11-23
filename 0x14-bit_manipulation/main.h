b303eb2aa0c822cf1ec6af2ec40dded4876c01ad
eric0149 <eriscorplock@gmail.com>
2023-11-23 23:23:53 +0300

    mofdified main to repush it

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,5 +11,4 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_indianness(void);
-
 #endif
