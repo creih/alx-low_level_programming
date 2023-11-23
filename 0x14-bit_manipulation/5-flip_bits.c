ae5bd3244391e68bb3577a2945e585144fedaabb
eric0149 <eriscorplock@gmail.com>
2023-11-23 23:07:49 +0300

    final task finally

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * flip bits - nbr of bits to change to have similar nbrs
+ * @n: nbr uno
+ * @m: dos numero
+ * Return: nbr of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bara = 0;
+	unsigned long int xor_res = n ^ m;
+
+	while (xor_res != 0)
+	{
+		bara += xor_res & 1;
+		xor_res >>= 1;
+	}
+	return (bara);
+}
