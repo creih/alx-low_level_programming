d1ac04a7d915c08de8c7fb931fbd93e37ad0c35c
eric0149 <eriscorplock@gmail.com>
2023-11-28 00:00:58 +0300

    100task

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_ikosa - outputs error
+ * @message: sms indicating error
+ */
+void print_ikosa(const char *message) {
+	fprintf(stderr, "%s\n", message);
+	exit(98);
+}
+/**
+ * print_elf_header_info - prints the elf header content
+ * @header: ptr to the header info
+ */
+void print_elf_header_info(Elf64_Ehdr *header) {
+	printf("  Magic:   ");
+	for (int i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x ", header->e_ident[i]);
+	}
+	printf("\n");   
+	printf("  Class:                             %s\n", (header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %d\n", header->e_ident[EI_OSABI]);
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %d\n", header->e_type);
+	printf("  Entry point address:               %lx\n", (unsigned long)header->e_entry);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,11 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
+#define BUFF_SIZE 64
+void print_ikosa(const char *message);
+void print_elf_header_info(ELF64_Ehdr *header);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 #endif
