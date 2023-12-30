d1ac7499d14cbeb4321f2d6591e5ef1b539d9929
cReih <122928197+creih@users.noreply.github.com>
2023-12-30 16:32:08 +0200

    advanced task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * disp_sms - displays the string message given before executing main()
+ * Return: nothing ata all
+ */
+void disp_sms(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
