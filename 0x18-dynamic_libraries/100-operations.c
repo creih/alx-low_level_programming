723a9b3cc8b10434fd7a25f0fefe31b499caca7f
creih <iherr.9@gmail.com>
2024-01-29 05:10:12 -0800

    this is 100 cpython

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 
-int add(int a, int b) {
+int add(int a, int b)
+{
     return a + b;
 }
 
-int sub(int a, int b) {
+int sub(int a, int b)
+{
     return a - b;
 }
 
-int mul(int a, int b) {
+int mul(int a, int b)
+{
     return (a * b);
 }
 
-int div(int a, int b) {
-    if (b != 0) {
+int div(int a, int b)
+{
+    if (b != 0)
         return (a / b);
-    } else {
+    else
+    {
         printf("Error: Division by zero\n");
         return (0);
     }
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Modulo by zero\n");
+		return 0;
+	}
+	return a % b;
+}
 }
