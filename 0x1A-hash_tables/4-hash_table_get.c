8c307c8e4225b449652bdf9c16b7d62e0364e078
creih <iherr.9@gmail.com>
2024-02-02 14:30:50 -0800

    task 3 betty solved

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,26 +5,22 @@
  * @ht: The hash table to look into
  * @key: The key to search for
  *
- * Return: The value associated with the key, or NULL if the key couldn't be found
+ * Return: value associated to key, or NULL if key couldn't be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *current;
+	unsigned long int index;
+	hash_node_t *current;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
-
-    index = key_index((const unsigned char *)key, ht->size);
-
-    current = ht->array[index];
-
-    while (current != NULL)
-    {
-        if (strcmp(current->key, key) == 0)
-            return (current->value); /* Key found, return the associated value */
-        current = current->next;
-    }
-
-    return (NULL); /* Key not found */
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL); /* Key not found */
 }
